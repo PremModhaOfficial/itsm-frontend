80fa408ec74341c4b1b3990c3fcc48f2097319f6
Pratvi Hirpara <pratvihirpara15@gmail.com>
2025-08-01 16:18:42 +0530

    pratvi

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { TechnicianProfilePage } from './pages/TechnicianProfilePage';
+import { TechnicianPerformancePage } from './pages/TechnicianPerformancePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/technician/:id" element={<TechnicianProfilePage />} />
+      <Route path="/technician/:id/performance" element={<TechnicianPerformancePage />} />
     </Routes>
   )
 }
