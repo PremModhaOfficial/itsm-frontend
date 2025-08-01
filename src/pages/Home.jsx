e6c0463eebaeffe084548ca34bcac4567f999515
prem modha <prem.modha@motadata.com>
2025-08-01 19:31:50 +0530

    pregunjan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { CallToAction } from "../sections/CallToAction";
-import {Features} from '../sections/Features';
+import { Features } from '../sections/Features';
 import { Footer } from "../sections/Footer";
 import { Header } from "../sections/Header";
 import { Hero } from "../sections/Hero";
@@ -8,35 +8,36 @@ import { LogoTicker } from "../sections/LogoTicker";
 import { Pricing } from "../sections/Pricing";
 import { ProductShowcase } from "../sections/ProductShowcase";
 import { Testimonials } from "../sections/Testimonials";
-import {Integrations} from "../sections/Integrations"
-import {Faqs} from '../sections/Faqs';
+import { Integrations } from "../sections/Integrations"
+import { Faqs } from '../sections/Faqs';
 import { LoginModal } from "../components/LoginModal";
 
 export const Home = () => {
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
-  const handleGetStartedClick = () => {
-    setIsLoginModalOpen(true);
-  };
+    const handleGetStartedClick = () => {
+        setIsLoginModalOpen(true);
+    };
 
-  const handleCloseLoginModal = () => {
-    setIsLoginModalOpen(false);
-  };
+    const handleCloseLoginModal = () => {
+        setIsLoginModalOpen(false);
+    };
 
-  return (
-    <div className='antialiased bg-[#EAEEFE]'>
-      <Header onGetStartedClick={handleGetStartedClick} />
-      <Hero onGetStartedClick={handleGetStartedClick} />
-      <LogoTicker />
-      <ProductShowcase />
-      <Pricing />
-      <Testimonials />
-      <CallToAction onGetStartedClick={handleGetStartedClick} />
-      <Footer />
-      <Features />
-      <Integrations/>
-      <Faqs />
-      <LoginModal isOpen={isLoginModalOpen} onClose={handleCloseLoginModal} />
-    </div>
-  );
+    return (
+        <div className='antialiased bg-[#EAEEFE]'>
+            <Header onGetStartedClick={handleGetStartedClick} />
+            <Hero onGetStartedClick={handleGetStartedClick} />
+            <LogoTicker />
+            <ProductShowcase />
+
+            <Pricing />
+            <Testimonials />
+            <CallToAction onGetStartedClick={handleGetStartedClick} />
+            <Footer />
+            <Features />
+            <Integrations />
+            <Faqs />
+            <LoginModal isOpen={isLoginModalOpen} onClose={handleCloseLoginModal} />
+        </div>
+    );
 }; 
