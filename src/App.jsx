2968a73eb417fa7e9c50d585a262237dca3dbd7e
prem modha <prem.modha@motadata.com>
2025-08-01 16:08:38 +0530

    pre gunjan pull

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,9 @@ import { Testimonials } from "./sections/Testimonials";
 import { Integrations } from "./sections/Integrations"
 import { Faqs } from './sections/Faqs';
 import { LoginModal } from "./components/LoginModal";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TicketForm from './components/TicketForm';
-import { About } from './pages/About';
 
-function Landing() {
+function App() {
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-
     const handleGetStartedClick = () => {
         setIsLoginModalOpen(true);
     };
@@ -46,21 +42,5 @@ function Landing() {
     )
 }
 
-
-function App() {
-
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Landing />} />
-                <Route path='/ticket' element={<TicketForm />} />
-                <Route path="/technician/:id" element={<TechnicianProfilePage />} />
-                <Route path='/about' element={<About />} />
-            </Routes>
-        </BrowserRouter>
-    )
-
-}
 
 export default App
