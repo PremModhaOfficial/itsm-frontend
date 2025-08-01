c0ab764b5c760c36d3054f628eb26b488954e636
Pratvi Hirpara <pratvihirpara15@gmail.com>
2025-08-01 15:21:30 +0530

    pratvi

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import './App.css'
-import { useState } from 'react';
-import { CallToAction } from "./sections/CallToAction";
-import {Features} from './sections/Features';
-import { Footer } from "./sections/Footer";
-import { Header } from "./sections/Header";
-import { Hero } from "./sections/Hero";
-import { LogoTicker } from "./sections/LogoTicker";
-import { Pricing } from "./sections/Pricing";
-import { ProductShowcase } from "./sections/ProductShowcase";
-import { Testimonials } from "./sections/Testimonials";
-import {Integrations} from "./sections/Integrations"
-import {Faqs} from './sections/Faqs';
-import { LoginModal } from "./components/LoginModal";
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { TechnicianProfilePage } from './pages/TechnicianProfilePage';
 
 function App() {
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setIsLoginModalOpen(true);
-  };
-
-  const handleCloseLoginModal = () => {
-    setIsLoginModalOpen(false);
-  };
-
   return (
     <div className='antialiased bg-[#EAEEFE]'>
       <Header onGetStartedClick={handleGetStartedClick} />
